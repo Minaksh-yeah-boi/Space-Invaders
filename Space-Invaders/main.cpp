931d822923fb9d48da7cc987f5e45fe5c85a7ffe
Minaksh-yeah-boi <minakshtondon@outlook.com>
2024-08-09 16:22:19 +0530

    3rd commit

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -26,8 +26,26 @@ int main()
         // Draw a circle
         sf::CircleShape circle(50); // Radius 50
         circle.setFillColor(sf::Color::Red);
-        circle.setPosition(400, 300); // Set position
+        circle.setPosition(300, 300); // Set position
         window.draw(circle);
+
+        sf::Texture outscal_texture;
+        outscal_texture.loadFromFile("assets/textures/outscal_logo.png");
+
+        sf::Sprite outscal_sprite;
+        outscal_sprite.setTexture(outscal_texture);
+
+        outscal_sprite.setPosition(200, 100); // Position
+        outscal_sprite.setRotation(45); // Rotation in degrees
+        outscal_sprite.setScale(0.5, 0.5); // Scale factor
+
+        window.draw(outscal_sprite);
+
+        sf::Font font;
+        font.loadFromFile("assets/fonts/OpenSans.ttf");
+        sf::Text text("SFML is awesome!", font, 50);
+        text.setFillColor(sf::Color::White);
+        window.draw(text);
 
         // Display whatever you draw
         window.display();
