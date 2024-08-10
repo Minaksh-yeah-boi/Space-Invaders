4119d52cfb0739d577ea3b58854429c7e431b983
Minaksh-yeah-boi <minakshtondon@outlook.com>
2024-08-10 11:31:54 +0530

    2nd commit in feature 2

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,33 +1,47 @@
-<<<<<<< Updated upstream
-
-int main()
-{
-=======
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
 class Player {
 private: // private Access Modifier
 
-    // private Properties
+    // Private Properties
     int health = 3;
     sf::Vector2f position = sf::Vector2f(200.0f, 100.0f);
+    int movement_speed = 5;
     int player_score = 0;
 
 public: // public Access Modifier
 
-    // public Properties
+    // Public Properties
     sf::Texture player_texture;
     sf::Sprite player_sprite;
-    int movement_speed = 5;
+
+    //Public Getter & Setter methods
+    int getScore() {
+        return player_score;
+    };
+
+    void setScore(int newScore) {
+        player_score = newScore;
+    };
+
+    //New methods
+    void takeDamage() {};
+    void move() {};
+    void shootBullets() {};
 };
 
 int main() {
-    // Define the video mode (dimensions)
-    sf::VideoMode videoMode = sf::VideoMode(800, 600);
 
-    // Create a window object with specific dimensions and a title
-    sf::RenderWindow window(videoMode, "SFML Window");
+    sf::VideoMode videoMode = sf::VideoMode(800, 600); // Define the video mode (dimensions)
+
+    sf::RenderWindow window(videoMode, "SFML Window");  // Create a window object
+
+    Player player;  // Create the player object
+
+    player.player_texture.loadFromFile("assets/textures/player_ship.png"); // Load the player ship texture
+
+    player.player_sprite.setTexture(player.player_texture);  // Set the player sprite variable 
 
     while (window.isOpen()) {
         sf::Event event;
@@ -37,14 +51,24 @@ int main() {
                 window.close();
         }
 
+        // Handle keyboard input
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            player.move();
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            player.move();
+        }
 
         // Clear the window
-        window.clear(sf::Color::Blue);
+        window.clear(sf::Color::Blue); // this code will set a blue background color (optional)
 
-        // Display whatever you draw
-        window.display();
-    }
+        player.player_sprite.setPosition(player.getPosition()); // Set the position of the player sprite
 
->>>>>>> Stashed changes
+        window.draw(player.player_sprite);    // Draw the player sprite
+
+        window.display(); // Display what was drawn
+
+    } // end while loop
+
     return 0;
 }
