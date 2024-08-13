56aa762ab4001c3ca690030632116064e64f171f
Minaksh-yeah-boi <minakshtondon@outlook.com>
2024-08-13 11:12:21 +0530

    Latest commit on feature 4

--- Space-Invaders/Header/Event/EventService.h
+++ Space-Invaders/Header/Event/EventService.h
@@ -3,6 +3,12 @@
 #include <SFML/Window/Event.hpp>
 
 namespace Event {
+	enum class ButtonState
+	{
+		PRESSED,
+		HELD,
+		RELEASED,
+	};
 
 	class EventService
 	{
@@ -10,11 +16,19 @@ namespace Event {
 		sf::Event game_event; //event var
 		sf::RenderWindow* game_window; //ptr to our game window
 
+		ButtonState left_mouse_button_state;
+		ButtonState right_mouse_button_state;
+		ButtonState left_arrow_button_state;
+		ButtonState right_arrow_button_state;
+		ButtonState A_button_state;
+		ButtonState D_button_state;
+
 		bool isGameWindowOpen();
 		bool gameWindowWasClosed(); //for the condition we already had - the title bar cross.
 		bool hasQuitGame(); //for our new 'ESC' condition
 
-
+		void updateMouseButtonsState(ButtonState& current_button_state, sf::Mouse::Button mouse_button);
+		void updateKeyboardButtonsState(ButtonState& current_button_state, sf::Keyboard::Key keyboard_button);
 
 	public:
 		EventService();
@@ -31,6 +45,9 @@ namespace Event {
 
 		bool pressedLeftMouseButton();
 		bool pressedRightMouseButton();
+
+		bool pressedAKey();
+		bool pressedDKey();
 	};
 
 }
